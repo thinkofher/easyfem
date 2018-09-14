2e2a67c96f9b2332b532714c334fa95bd61c2817
Your name <you@example.com>
2018-09-15 00:07:31 +0200

    making code more readable

--- easybeam/easybeam.py
+++ easybeam/easybeam.py
@@ -44,10 +44,22 @@ class Beam:
     # 1D Beam Finite Element
 
     def __init__(self, length, youngs_modulus=1, section=False):
+
         self.length = length  # unit: m
         self.youngs_modulus = youngs_modulus
         self.section = section  # units: m
         self.stifness()
+
+        # some default definitons
+        self.boundaries(
+            True,
+            False,
+            True,
+            False
+        )
+        self.loads(
+            linear_load=1000
+        )
 
     def __repr__(self):
 
@@ -62,11 +74,11 @@ class Beam:
     def stifness(self):
 
         if self.section:
-            self.area = self.section.area  # unit: m^2
-            self.moment_of_inertia_y = self.section.moment_of_inertia_y  # unit: m^4
-            self.moment_of_inertia_z = self.section.moment_of_inertia_z  # unit: m^4
-            self.elastic_modulus_y = self.section.elastic_modulus_y  # unit: m^3
-            self.elastic_modulus_z = self.section.elastic_modulus_z  #  unit m^3 
+            self.area = self.section.area  # m^2
+            self.moment_of_inertia_y = self.section.moment_of_inertia_y  # m^4
+            self.moment_of_inertia_z = self.section.moment_of_inertia_z  # m^4
+            self.elastic_modulus_y = self.section.elastic_modulus_y  # m^3
+            self.elastic_modulus_z = self.section.elastic_modulus_z  # m^3
         else:
             self.area = 1  # unit: m^2
             self.moment_of_inertia_y = 1  # unit: m^4
@@ -75,20 +87,22 @@ class Beam:
             self.elastic_modulus_z = 1  # unit m^3
 
         try:
-            self.stifness_matrix =  ( ( 2*self.youngs_modulus*self.moment_of_inertia_z )/(self.length**3) ) * np.array([[ 6, 3*self.length, -6, 3*self.length ], [3*self.length, 2*self.length**2, -3*self.length, self.length**2], [ -6, -3*self.length, 6, -3*self.length ], [ 3*self.length, self.length**2, -3*self.length, 2*self.length**2 ]])
+            self.stifness_matrix = ((2*self.youngs_modulus*self.moment_of_inertia_z)/(self.length**3)) * np.array([[6, 3*self.length, -6, 3*self.length], [3*self.length, 2*self.length**2, -3*self.length, self.length**2], [-6, -3*self.length, 6, -3*self.length], [3*self.length, self.length**2, -3*self.length, 2*self.length**2]])
         except:
             raise No_Data
-        
+
     def boundary(self, vertical_1, rotation_1, vertical_2, rotation_2):
         self.vertical_1 = bool(vertical_1)
         self.rotation_1 = bool(rotation_1)
         self.vertical_2 = bool(vertical_2)
         self.rotation_2 = bool(rotation_2)
-        self.boundaries = [ self.vertical_1,
-                            self.rotation_1,
-                            self.vertical_2,
-                            self.rotation_2]
-    
+        self.boundaries = [
+            self.vertical_1,
+            self.rotation_1,
+            self.vertical_2,
+            self.rotation_2
+            ]
+
     def loads(self, force_1=0, force_2=0, moment_1=0, moment_2=0, linear_load=0):
         try:
             self.linear_load = linear_load
@@ -104,25 +118,35 @@ class Beam:
             self.moment_1 = moment_1 + self.linear_load*(self.length**2)*(1/12)
             self.moment_2 = moment_2 - self.linear_load*(self.length**2)*(1/12)
 
-            self.system_loads = np.array([self.force_1, self.moment_1, self.force_2, self.moment_2])
+            self.system_loads = np.array(
+                [self.force_1, self.moment_1, self.force_2, self.moment_2]
+                )
         except:
             raise No_Data
 
     def internal_forces(self, solved_forces):
         self.__solved_forces__ = solved_forces
-        self.internal_forces_array =  (-1)*np.transpose(np.matrix(self.system_loads)) + self.stifness_matrix * np.transpose(np.matrix(self.__solved_forces__))
-        self.internal_forces_array = np.array( np.transpose( self.internal_forces_array ) )[0]
-        
+        self.internal_forces_array = \
+            (-1)*np.transpose(np.matrix(self.system_loads)) + \
+            self.stifness_matrix * np.transpose(
+                np.matrix(self.__solved_forces__)
+                )
+
+        self.internal_forces_array = np.array(
+            np.transpose(self.internal_forces_array)
+            )[0]
+
     def solve(self):
         return self.__solved_forces__
+
 
 class BeamSolver:
 
     def __init__(self, *beams):
         self.beams = tuple(chain.from_iterable(beams))
-        
+
         self.n = 4 + (len(self.beams)-1)*2
-        
+
         self.internal_agregation()
         self.internal_boundaries()
         self.internal_system_loads()
@@ -133,13 +157,14 @@ class BeamSolver:
 
         matrixes = [matrix.stifness_matrix for matrix in self.beams]
 
-        self.global_stiffness_matrix = np.zeros([self.n,self.n])
+        self.global_stiffness_matrix = np.zeros([self.n, self.n])
 
         for i in range(len(matrixes)):
-            current_matrix = np.zeros([self.n,self.n])
-            current_matrix[(0+2*i):(4+2*i),(0+2*i):(4+2*i)] = matrixes[i]
-            self.global_stiffness_matrix = self.global_stiffness_matrix + current_matrix
-    
+            current_matrix = np.zeros([self.n, self.n])
+            current_matrix[(0+2*i):(4+2*i), (0+2*i):(4+2*i)] = matrixes[i]
+            self.global_stiffness_matrix = \
+                self.global_stiffness_matrix + current_matrix
+
     def internal_boundaries(self):
 
         vectors = [np.array(beam.boundaries) for beam in self.beams]
@@ -149,9 +174,12 @@ class BeamSolver:
         for i in range(len(vectors)):
             current_vector = np.zeros([self.n])
             current_vector[(0+2*i):(4+2*i)] = vectors[i]
-            self.global_boundary_vector = self.global_boundary_vector + current_vector
-        
-        self.global_boundary_vector = np.array(list(map(bool,self.global_boundary_vector)))
+            self.global_boundary_vector = \
+                self.global_boundary_vector + current_vector
+
+        self.global_boundary_vector = np.array(
+            list(map(bool, self.global_boundary_vector))
+            )
 
     def internal_system_loads(self):
 
@@ -162,41 +190,60 @@ class BeamSolver:
         for i in range(len(vectors)):
             current_vector = np.zeros([self.n])
             current_vector[(0+2*i):(4+2*i)] = vectors[i]
-            self.global_system_loads_vector = self.global_system_loads_vector + current_vector
+            self.global_system_loads_vector = \
+                self.global_system_loads_vector + current_vector
 
-    def apply_boundaries(self): 
+    def apply_boundaries(self):
 
         self.boundariezed_stiffness_matrix = self.global_stiffness_matrix
         self.boundariezed_system_loads_vector = self.global_system_loads_vector
         self.__solved_vector__ = np.zeros([self.n])
 
-        delete_counter  = 0
+        delete_counter = 0
 
-        for i in range(np.size(self.global_boundary_vector,0)):
+        for i in range(np.size(self.global_boundary_vector, 0)):
             if self.global_boundary_vector[i]:
-                self.boundariezed_stiffness_matrix = np.delete(self.boundariezed_stiffness_matrix, delete_counter, 0)
-                self.boundariezed_stiffness_matrix = np.delete(self.boundariezed_stiffness_matrix, delete_counter, 1)
-                self.boundariezed_system_loads_vector = np.delete(self.boundariezed_system_loads_vector, delete_counter) 
+                self.boundariezed_stiffness_matrix = np.delete(
+                    self.boundariezed_stiffness_matrix,
+                    delete_counter,
+                    0
+                    )
+                self.boundariezed_stiffness_matrix = np.delete(
+                    self.boundariezed_stiffness_matrix,
+                    delete_counter,
+                    1
+                    )
+                self.boundariezed_system_loads_vector = np.delete(
+                    self.boundariezed_system_loads_vector,
+                    delete_counter
+                    )
             else:
                 delete_counter += 1
                 self.__solved_vector__[i] = True
 
     def solver(self):
-        self.unknowns_solved = lp.solve(self.boundariezed_stiffness_matrix, self.boundariezed_system_loads_vector)
+        self.unknowns_solved = lp.solve(
+            self.boundariezed_stiffness_matrix,
+            self.boundariezed_system_loads_vector
+            )
         self.global_solvings_vector = np.zeros([self.n])
 
         unknowns_counter = 0
         for i in range(len(self.__solved_vector__)):
             if self.__solved_vector__[i]:
-                self.global_solvings_vector[i] = self.unknowns_solved[unknowns_counter]
+                self.global_solvings_vector[i] = \
+                    self.unknowns_solved[unknowns_counter]
                 unknowns_counter += 1
-        
-        for counter,beam_element in enumerate(self.beams):
-            beam_element.internal_forces(self.global_solvings_vector[(0+2*counter):(4+2*counter)])
-    
+
+        for counter, beam_element in enumerate(self.beams):
+            beam_element.internal_forces(
+                self.global_solvings_vector[(0+2*counter):(4+2*counter)]
+                )
+
     def results(self):
 
         return self.beams
+
 
 def discretization(beam, number_of_elements):
     digitized_beam = []
@@ -205,24 +252,42 @@ def discretization(beam, number_of_elements):
     for i in range(number_of_elements):
         # tworze tymczasowy element do dyskretyzacji
         # o klasie Beam
-        current_beam = Beam(length_of_element, beam.youngs_modulus, beam.section)
+        current_beam = Beam(
+            length_of_element,
+            beam.youngs_modulus,
+            beam.section
+            )
 
         # okreslam warunki brzegowe poszczegolnych elementow
         # i nadaje obciazenia
         if i == 0:
             current_beam.boundary(beam.vertical_1, beam.rotation_1, False, False)
-            current_beam.loads(force_1=beam.init_force_1,moment_1=beam.init_moment_1,linear_load=beam.linear_load)
+            current_beam.loads(
+                force_1=beam.init_force_1,
+                moment_1=beam.init_moment_1,
+                linear_load=beam.linear_load
+                )
         elif i == (number_of_elements-1):
-            current_beam.boundary(False, False, beam.vertical_2, beam.rotation_2)
-            current_beam.loads(force_2=beam.init_force_2,moment_2=beam.init_moment_2,linear_load=beam.linear_load)
+            current_beam.boundary(
+                False,
+                False,
+                beam.vertical_2,
+                beam.rotation_2
+                )
+            current_beam.loads(
+                force_2=beam.init_force_2,
+                moment_2=beam.init_moment_2,
+                linear_load=beam.linear_load
+                )
         else:
             current_beam.loads(linear_load=beam.linear_load)
             current_beam.boundary(False, False, False, False)
-        
+
         # dodaje gotowy element do listy
         digitized_beam.append(current_beam)
-    
+
     return tuple(digitized_beam)
+
 
 def momments_array(*beams):
     # fucntions for crating arrays with
@@ -231,11 +296,15 @@ def momments_array(*beams):
 
     moments_array = []
     for beam in beams:
-        moments = [ beam.internal_forces_array[1], -1*beam.internal_forces_array[3] ]
+        moments = [
+            beam.internal_forces_array[1],
+            -1*beam.internal_forces_array[3]
+            ]
         for moment in moments:
             moments_array.append(moment)
 
     return np.array(moments_array)
+
 
 def shears_array(*beams):
     # fucntions for crating arrays with
@@ -244,33 +313,39 @@ def shears_array(*beams):
 
     shears_array = []
     for beam in beams:
-        shears = [ beam.internal_forces_array[0], -1*beam.internal_forces_array[2] ]
+        shears = [
+            beam.internal_forces_array[0],
+            -1*beam.internal_forces_array[2]
+            ]
         for shear in shears:
             shears_array.append(shear)
 
     return np.array(shears_array)
+
 
 def disps_array(*beams):
     beams = tuple(chain.from_iterable(beams))
 
     disps_array = []
     for beam in beams:
-        disps = [ beam.solve()[0], beam.solve()[2] ]
+        disps = [beam.solve()[0], beam.solve()[2]]
         for disp in disps:
             disps_array.append(disp)
 
     return np.array(disps_array)
+
 
 def rotations_array(*beams):
     beams = tuple(chain.from_iterable(beams))
 
     rot_array = []
     for beam in beams:
-        rots = [ beam.solve()[1], beam.solve()[3] ]
+        rots = [beam.solve()[1], beam.solve()[3]]
         for rot in rots:
             rot_array.append(rot)
 
     return np.array(rot_array)
+
 
 def coordinates_array(*beams):
     # fucntions for crating arrays with
